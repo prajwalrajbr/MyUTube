db1ca5a8308e9b532c060e4d55f3d03c741dd245
prajwalrajbr <42075029+prajwalrajbr@users.noreply.github.com>
2020-05-30 18:03:06 +0530

    Done on sidenav.

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+});
+
 const CLIENT_ID = '280319480833-i4chh7eknrp3eudjbffr2hbgq4lgjrd4.apps.googleusercontent.com';
 const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"];
 const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
 
 const authorizeButton = document.getElementById('authorize-button');
+const authorizeButtonNav = document.getElementById('authorize-button-sidenav');
 const signoutButton = document.getElementById('signout-button');
+const signoutButtonNav = document.getElementById('signout-button-sidenav');
 
 const content = document.getElementById('content');
 const channelForm = document.getElementById('channel-form');
@@ -22,28 +28,38 @@ function initClient() {
         clientId: CLIENT_ID,
         scope: SCOPES
     }).then(()=>{
+        gapi.client.setApiKey("AIzaSyC86qGosUbBF9ehKaV0SJh7m8AwVy3m-ww");
+        gapi.client.load(DISCOVERY_DOCS[0])
         gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
         updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
         authorizeButton.onclick = handleAuthClick;
+        authorizeButtonNav.onclick = handleAuthClick;
         signoutButton.onclick = handleSignoutClick;
+        signoutButtonNav.onclick = handleSignoutClick;
     });
 }
 
 function updateSigninStatus(isSignedIn){
     if (isSignedIn){
         authorizeButton.style.display = 'none';
-        signoutButton.style.display = 'block';
-        content.style.display = 'block';
+        authorizeButtonNav.style.display = 'none';
+        signoutButton.style.display = 'inline-block';
+        signoutButtonNav.style.display = 'inline-block';
+        content.style.display = 'inline-block';
+        document.getElementById('google-prompt').style.display = 'none';        
         getChannel(defaultChannel);
     }else{
-        authorizeButton.style.display = 'block';
+        authorizeButton.style.display = 'inline-block';
+        authorizeButtonNav.style.display = 'inline-block';
+        document.getElementById('google-prompt').style.display = 'block'; 
         signoutButton.style.display = 'none';
+        signoutButtonNav.style.display = 'none';
         content.style.display = 'none';
     }
 }
 
 function handleAuthClick(){
-    gapi.auth2.getAuthInstance().signIn();
+    gapi.auth2.getAuthInstance().signIn({scope: SCOPES});
 }
 
 function handleSignoutClick(){
