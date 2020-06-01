59244abb392debf741ae5928df42ec11c6c433e8
prajwalrajbr <42075029+prajwalrajbr@users.noreply.github.com>
2020-06-01 12:06:46 +0530

    Done with displaying without login videos

--- index.js
+++ index.js
@@ -6,65 +6,89 @@ $(document).ready(function(){
   $('select').formSelect();
 });
 
-var staticUrl = '';
 var orderBy = "rating";
 var catagoryID = "28";
+var videoCount = 0;
+var staticUrl = 'https://www.googleapis.com/youtube/v3/search?order='+orderBy+'&part=snippet&maxResults='+(videoCount+5)+'&type=video&videoCategoryId='+catagoryID+'&key=AIzaSyC86qGosUbBF9ehKaV0SJh7m8AwVy3m-ww&alt=json';
+$.getJSON(staticUrl, function(data) {
+    Array.prototype.forEach.call( document.querySelectorAll('#video-row'), function( node ) {
+        node.parentNode.removeChild( node );
+    });
+    loadVideos(data);
+});
 
 document.getElementById('order-list').onchange = function () {
     console.log(document.getElementById('order-list').value);
     orderBy = document.getElementById('order-list').value;
-    staticUrl = 'https://www.googleapis.com/youtube/v3/search?order='+orderBy+'&part=snippet&type=video&videoCategoryId='+catagoryID+'&key=AIzaSyC86qGosUbBF9ehKaV0SJh7m8AwVy3m-ww&alt=json';
+    staticUrl = 'https://www.googleapis.com/youtube/v3/search?order='+orderBy+'&part=snippet&maxResults='+(videoCount+5)+'&type=video&videoCategoryId='+catagoryID+'&key=AIzaSyC86qGosUbBF9ehKaV0SJh7m8AwVy3m-ww&alt=json';
     $.getJSON(staticUrl, function(data) {
-        var z = document.querySelector('#without-login');
-        var toInsertBefore = document.querySelector('#End');
         Array.prototype.forEach.call( document.querySelectorAll('#video-row'), function( node ) {
             node.parentNode.removeChild( node );
         });
-        for (i=0;i<data.items.length;i++) {
-            var newElement = document.createElement('div');
-            newElement.className = 'row';
-            newElement.id = 'video-row';
-
-            var newSElement = document.createElement('div');
-            newSElement.className = 'col s1';
-
-            var newVElement = document.createElement('div');
-            newVElement.className = 'col s10';
-            var newVideoElement = document.createElement('div');
-            newVideoElement.className = 'video-container';
-
-            var iframe = document.createElement('iframe');
-            iframe.src = "https://www.youtube.com/embed/"+data.items[i].id.videoId;
-            iframe.frameBorder = "0";
-            iframe.allow = "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture";
-            iframe.setAttribute('allowfullscreen', true);
-
-            newVideoElement.appendChild(iframe);
-
-            newVElement.appendChild(newVideoElement);
-
-            var newEElement = document.createElement('div');
-            newEElement.className = 'col s1';
-
-            newElement.appendChild(newEElement);
-            newElement.appendChild(newVElement);
-            newElement.appendChild(newSElement);
-
-            z.insertBefore(newElement,toInsertBefore)
-        }
+        loadVideos(data);
     });
 };
 
 document.getElementById('catagory-list').onchange = function () {
     console.log(document.getElementById('catagory-list').value);
     catagoryID = document.getElementById('catagory-list').value;
-    staticUrl = 'https://www.googleapis.com/youtube/v3/search?order='+orderBy+'&part=snippet&type=video&videoCategoryId='+catagoryID+'&key=AIzaSyC86qGosUbBF9ehKaV0SJh7m8AwVy3m-ww&alt=json';
+    staticUrl = 'https://www.googleapis.com/youtube/v3/search?order='+orderBy+'&part=snippet&maxResults='+(videoCount+5)+'&type=video&videoCategoryId='+catagoryID+'&key=AIzaSyC86qGosUbBF9ehKaV0SJh7m8AwVy3m-ww&alt=json';
     $.getJSON(staticUrl, function(data) {
         Array.prototype.forEach.call( document.querySelectorAll('#video-row'), function( node ) {
             node.parentNode.removeChild( node );
         });
+        loadVideos(data);
     });
 };
+
+function loadVideos(data){
+    for (i=videoCount;i<data.items.length;i++) {
+        var z = document.querySelector('#without-login');
+        var toInsertBefore = document.querySelector('#End');
+        
+        var newElement = document.createElement('div');
+        newElement.className = 'row';
+        newElement.id = 'video-row';
+
+        var newSElement = document.createElement('div');
+        newSElement.className = 'col s1';
+
+        var newVElement = document.createElement('div');
+        newVElement.className = 'col s10';
+        var newVideoElement = document.createElement('div');
+        newVideoElement.className = 'video-container';
+
+        var iframe = document.createElement('iframe');
+        iframe.src = "https://www.youtube.com/embed/"+data.items[i].id.videoId;
+        iframe.frameBorder = "0";
+        iframe.allow = "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture";
+        iframe.setAttribute('allowfullscreen', true);
+
+        newVideoElement.appendChild(iframe);
+
+        newVElement.appendChild(newVideoElement);
+
+        var newEElement = document.createElement('div');
+        newEElement.className = 'col s1';
+
+        newElement.appendChild(newEElement);
+        newElement.appendChild(newVElement);
+        newElement.appendChild(newSElement);
+
+        z.insertBefore(newElement,toInsertBefore)
+    }
+}
+
+document.getElementById('load-more').onclick = function (){
+    videoCount += 5;
+    staticUrl = 'https://www.googleapis.com/youtube/v3/search?order='+orderBy+'&part=snippet&maxResults='+(videoCount+5)+'&type=video&videoCategoryId='+catagoryID+'&key=AIzaSyC86qGosUbBF9ehKaV0SJh7m8AwVy3m-ww&alt=json';
+    $.getJSON(staticUrl, function(data) {
+        loadVideos(data);
+    });
+    if(videoCount==45){
+        document.getElementById('load-more').className = 'btn disabled';
+    }
+}
 
 //https://www.googleapis.com/youtube/v3/search?order=viewCount&part=snippet&type=video&videoCategoryId=34&key=AIzaSyC86qGosUbBF9ehKaV0SJh7m8AwVy3m-ww&alt=json
 
@@ -112,7 +136,8 @@ function updateSigninStatus(isSignedIn){
         signoutButton.style.display = 'inline-block';
         signoutButtonNav.style.display = 'inline-block';
         content.style.display = 'inline-block';
-        document.getElementById('google-prompt').style.display = 'none';        
+        document.getElementById('google-prompt').style.display = 'none'; 
+        document.getElementById('without-login').style.display = 'none';
         getChannel(defaultChannel);
     }else{
         authorizeButton.style.display = 'inline-block';
@@ -121,6 +146,7 @@ function updateSigninStatus(isSignedIn){
         signoutButton.style.display = 'none';
         signoutButtonNav.style.display = 'none';
         content.style.display = 'none';
+        document.getElementById('without-login').style.display = 'block';
     }
 }
 
